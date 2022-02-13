36e1256bc74ad4e798d8712e02c7f04e30885343
Rodrigo <rodrigo.cdpadua@gmail.com>
2022-02-13 13:42:22 -0300

    ADJUST: Database Functions and api_req

---
+++ api/api_req.js
@@ -0,0 +1,14 @@
+import fetch from "node-fetch";
+
+const url = 'https://servicodados.ibge.gov.br/api/v1/paises';
+
+async function getCountries() {
+    return (
+        await fetch(url)
+        .then(res => res.json())
+        .then(countries => countries)
+        .catch(err => console.log('Error'))
+    );
+}
+
+export default getCountries;
--- db/database.js
+++ db/database.js
@@ -12,69 +12,117 @@ async function createTables() {
     await client.connect();
     await client.query(
         `
-        CREATE TABLE IF NOT EXISTS "projects" (
+        CREATE TABLE IF NOT EXISTS "subgroups" (
             id SERIAL PRIMARY KEY,
-            name VARCHAR(100) NOT NULL,
-            description TEXT NOT NULL
+            name VARCHAR(50) NOT NULL,
+            status BOOLEAN DEFAULT TRUE,
+            qnt_countries SMALLINT DEFAULT 0,
+            creation_date DATE DEFAULT CURRENT_DATE
+        );
+
+        CREATE TABLE IF NOT EXISTS "countries" (
+            id SMALLINT PRIMARY KEY NOT NULL UNIQUE,
+            acronym VARCHAR(2) NOT NULL,
+            name VARCHAR(50) NOT NULL,
+            language VARCHAR(20) NOT NULL,
+            capital VARCHAR(50) NOT NULL,
+            coin VARCHAR(20) NOT NULL
         );
         
-        CREATE TABLE IF NOT EXISTS "projectscountries" (
+        CREATE TABLE IF NOT EXISTS "subgroupscountries" (
             id SERIAL PRIMARY KEY,
-            id_project INTEGER,
-            id_country INTEGER,
-            FOREIGN KEY (id_project) REFERENCES projects (id) ON DELETE CASCADE
+            id_subgroup INTEGER,
+            id_country SMALLINT,
+            FOREIGN KEY (id_subgroup) REFERENCES subgroups (id) ON DELETE CASCADE,
+            FOREIGN KEY (id_country) REFERENCES countries (id) ON DELETE CASCADE
         );
         `
     );
     client.end();
 }
 
-async function insertProjects(project) {
+async function insertSubGroup(group) {
     await client.connect();
     await client.query(
         `
-        INSERT INTO projects VALUES (
+        INSERT INTO subgroups (id, name) VALUES (
             DEFAULT,
-            '${project.name}',
-            '${project.description}'
+            '${group.name}'
         );
         `
     );
     client.end();
 }
 
-async function insertProjectCountry(idProjectCountry) {
+async function insertCountry(country) {
     await client.connect();
     await client.query(
         `
-        INSERT INTO projectscountries VALUES (
+        INSERT INTO countries VALUES (
+            '${country.id}',
+            '${country.acronym}',
+            '${country.name}',
+            '${country.lenguage}',
+            '${country.capital}',
+            '${country.coin}'
+        );
+        `
+    );
+    client.end();
+}
+
+async function insertSubGroupsCountries(idGroupCountry) {
+    await client.connect();
+    await client.query(
+        `
+        INSERT INTO groupscountries VALUES (
             DEFAULT,
-            '${idProjectCountry.idProject}',
-            '${idProjectCountry.idCountry}'
+            '${idGroupCountry.idSubGroup}',
+            '${idGroupCountry.idCountry}'
         );
         `
     )
     client.end();
 }
 
-async function deleteProject(idProject) {
+async function deleteSubGroup(idSubGroup) {
     await client.connect();
     await client.query(
         `
-        DELETE FROM projects
-            WHERE id = '${idProject}'
+        DELETE FROM subgroups
+            WHERE id = '${idSubGroup}'
         `
     );
     client.end();
 }
 
-async function deleteProjectCountry(idProjectCountry) {
+async function deleteCountry(idCountry) {
     await client.connect();
     await client.query(
         `
-        DELETE FROM projectscountries
-            WHERE id_project = '${idProjectCountry.idProject}' AND id_country = '${idProjectCountry.idCountry}'
+        DELETE FROM countries
+            WHERE id = '${idCountry}'
         `
     );
     client.end();
 }
+
+async function deleteSubGroupCountry(idGroupCountry) {
+    await client.connect();
+    await client.query(
+        `
+        DELETE FROM groupscountries
+            WHERE id = '${idGroupCountry}'
+        `
+    );
+    client.end();
+}
+
+//createTables();
+//insertSubGroup({name: ''});
+//const c = {id: , acronym: '', name: '', lenguage: '', capital: '', coin: ''};
+//insertCountry(c);
+//insertSubGroupsCountries({idSubGroup: , idCountry: });
+//deleteSubGroupCountry();
+//deleteCountry();
+//deleteSubGroup();
